d6ea94a003f4a2b451a9a5bfb6aff6afbc939dff
ibaezat <ignacio.baeza@r1vs.com>
2025-10-15 22:38:07 -0300

    first commit hehe

---
+++ src/main/java/com/taptag/backend/StatusController.java
@@ -0,0 +1,38 @@
+package com.taptag.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+@RestController
+public class StatusController {
+
+    private final List<String> greetings = List.of(
+            "¡Hola!",
+            "¡Qué bueno verte por aquí!",
+            "¡Bienvenido a TapTag! 🚀",
+            "¡Saludos desde el backend!",
+            "Hello!",
+            "Good Morning!",
+            ":)",
+            "Halo",
+            "Oi!",
+            "Hoooola!"
+    );
+
+    private final Random random = new Random();
+
+    @GetMapping("/api/status")
+    public Map<String, String> status(){
+        return Map.of("status", "OK");
+    }
+
+    @GetMapping("/api/saludo")
+    public Map<String, String> getGreeting() {
+        String greeting = greetings.get(random.nextInt(greetings.size()));
+        return Map.of("message", greeting);
+    }
+
+}
---
+++ src/test/java/com/taptag/backend/TapTagBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taptag.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapTagBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
